a52fb062ac521924be16024a0038bcb5c5ed747e
Sherryjsph3 <83832504+Sherryjsph3@users.noreply.github.com>
2023-02-13 18:38:45 -0500

    added loading animation

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Households from './Households.js';
+import * as SVGLoaders from 'svg-loaders-react';
 
 function Data(props) {
-  return props.members.map((member, index) => (
-    <Households {...member} key={index} />
-  ));//passed down the prop (members) from main.js
+  const loaded = () => {
+    return props.members.map((member, index) => (
+      <Households {...member} key={index} />
+    )); //passed down the prop (members) from main.js
+  };
+  const loading = () => {
+    return <SVGLoaders.Puff />;
+  };
+
+  return props.members ? loaded() : loading();
 }
 
 export default Data;
